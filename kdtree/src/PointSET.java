e151378e8e0fba28032f4f5a0dea4b020fc2244c
Zhi LIU <liuzhi0210@gmail.com>
2017-04-30 08:37:27 +0200

    kdtree initialize

---
+++ kdtree/src/PointSET.java
@@ -0,0 +1,92 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class PointSET {
+
+    private TreeSet<Point2D> points;
+
+    // construct an empty set of points
+    public PointSET() {
+        points = new TreeSet<>();
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return points.isEmpty();
+    }
+
+    // number of points in the set
+    public int size() {
+        return points.size();
+    }
+
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        if (p == null) {
+            throw new NullPointerException();
+        }
+        points.add(p);
+    }
+
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        if (p == null) {
+            throw new NullPointerException();
+        }
+        return points.contains(p);
+    }
+
+    // draw all points to standard draw
+    public void draw() {
+        for (Point2D point:
+             points) {
+            point.draw();
+        }
+    }
+
+    // all points that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) {
+            throw new NullPointerException();
+        }
+        ArrayList<Point2D> pts = new ArrayList<>();
+        for (Point2D pt :
+                points) {
+            if (rect.contains(pt)) {
+                pts.add(pt);
+            }
+        }
+        return pts;
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        if (p == null) {
+            throw new NullPointerException();
+        }
+        if (isEmpty()) {
+            return null;
+        }
+        Point2D neighbor = null;
+        double minDist = Double.MIN_VALUE;
+        Iterator<Point2D> iterator = points.iterator();
+        while (iterator.hasNext()) {
+            Point2D d = iterator.next();
+            double dist = d.distanceSquaredTo(p);
+            if (dist < minDist) {
+                minDist = dist;
+                neighbor = d;
+            }
+        }
+        return neighbor;
+    }
+
+    // unit testing of the methods (optional)
+    public static void main(String[] args) {
+
+    }
+}
